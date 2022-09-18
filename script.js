277e0f174b108b4990900931a8b542de51322837
Dzmitry <nakadisa@icloud.com>
2022-09-18 18:45:47 +0200

    Add functions

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 'use strict';
 
-const numberOfFilms = +prompt('Проверка редактирования. Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
+function start() {
+    numberOfFilms = +prompt('Проверка редактирования. Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) { 
+        //null - проверяет не нажал ли ОТМЕНА, isNaN (is not a number) - прворяет является ли то что ввел числом.
+        numberOfFilms = +prompt('Проверка редактирования. Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,56 +17,51 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-//Через Цикл While
-/*
-let m = 0;
-while (m<2) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-         if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-            personalMovieDB.movies[a] = b;
-         } else {
-            m--;
-         }
-         m++;
-}
-*/
-//Через цикл For
-/*for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-         if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-            personalMovieDB.movies[a] = b;
-         } else {
-            i--;
-         }
-        
-}
-*/
-//Через цикл do while (не решил)
-let m = 0;
-do {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
-         if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-            personalMovieDB.movies[a] = b;
-         } else {
-            m--;
-         }
-         m++;
-}
-while (m<2);
-
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель'); 
-} else  if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Ошибка');
-}
-console.log(personalMovieDB);
 
 
 
+function rememberMyFilms () {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+             if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+                personalMovieDB.movies[a] = b;
+             } else {
+                i--;
+             }
+            
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel () {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель'); 
+    } else  if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Ошибка');
+    }
+}
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres () {
+    for (let i = 1; i <= 3; i++) {
+        let qiz = prompt (`Ваш любимый жанр под номером ${i}`,'');
+        while (qiz == '' || qiz == null || typeof(qiz) == 'number') {
+            qiz = prompt (`Ваш любимый жанр под номером ${i}`,'');
+        }
+        personalMovieDB.genres[i-1] = qiz;
+    }
+}
+writeYourGenres();
